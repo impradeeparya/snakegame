5ecb9687cfd9c8670cce14d544fa229147d7a95c
Pradeep Arya <impradeeparya@gmail.com>
2015-06-22 22:56:57 +0530

    backgroundmusic and boundary rule

--- src/com/mysnakegame/GameBoard.java
+++ src/com/mysnakegame/GameBoard.java
@@ -5,10 +5,17 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JPanel;
 
 public class GameBoard extends JPanel implements Runnable {
@@ -16,11 +23,12 @@ public class GameBoard extends JPanel implements Runnable {
 	private static final long serialVersionUID = 5364147787150299178L;
 
 	private Thread snakeThread;
+	private Thread backgroundMusicThread;
 
-	private boolean rightMove = false;
-	private boolean leftMove = false;
-	private boolean upMove = true;
-	private boolean downMove = false;
+	private boolean rightMove;
+	private boolean leftMove;
+	private boolean upMove;
+	private boolean downMove;
 
 	private int targetX;
 	private int targetY;
@@ -42,27 +50,63 @@ public class GameBoard extends JPanel implements Runnable {
 	private static Configurations config = Configurations.getInstance();
 
 	public GameBoard() {
-		
+
+		setBackground(Color.green);
+
 		BOARD_WIDTH = Integer.parseInt(config.getProperty("BOARD_WIDTH"));
 		BOARD_HEIGHT = Integer.parseInt(config.getProperty("BOARD_HEIGHT"));
-		
-		BOARD_LOWER_LIMIT = Integer.parseInt(config.getProperty("BOARD_LOWER_LIMIT"));
-		BOARD_UPPER_LIMIT = Integer.parseInt(config.getProperty("BOARD_UPPER_LIMIT"));
-		
+
+		BOARD_LOWER_LIMIT = Integer.parseInt(config
+				.getProperty("BOARD_LOWER_LIMIT"));
+		BOARD_UPPER_LIMIT = Integer.parseInt(config
+				.getProperty("BOARD_UPPER_LIMIT"));
+
 		snakeX = Integer.parseInt(config.getProperty("SNAKE_X"));
 		snakeY = Integer.parseInt(config.getProperty("SNAKE_Y"));
-		
+
 		BOX_SIZE = Integer.parseInt(config.getProperty("BOX_SIZE"));
-		
+
 		setFocusable(true);
 		addKeyListener(new SnakeMovements());
 		setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
+		setDefaultDirection();
 		startGame();
+	}
+
+	private void setDefaultDirection() {
+		rightMove = false;
+		leftMove = false;
+		upMove = true;
+		downMove = false;
 	}
 
 	private void startGame() {
 		snake = new ArrayList<SnakeBody>();
 		setTargetLocation();
+
+		backgroundMusicThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true)
+					try {
+						Clip backgroundMusic = AudioSystem.getClip();
+						AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(
+								"backgroundmusic.wav"));
+						backgroundMusic.open(inputStream);
+						backgroundMusic.start();
+						Thread.sleep(10000);
+					} catch (LineUnavailableException e) {
+						e.printStackTrace();
+					} catch (UnsupportedAudioFileException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+			}
+		}, "Game Background Music");
+		backgroundMusicThread.start();
 		snakeThread = new Thread(this, "Snake Game");
 		snakeThread.start();
 	}
@@ -91,7 +135,14 @@ public class GameBoard extends JPanel implements Runnable {
 			checkMovement();
 			time = 0;
 			snake.add(new SnakeBody(snakeX, snakeY, BOX_SIZE, BOX_SIZE));
-			if ((snakeX == targetX) && ((snakeY == targetY))) {
+			if ((snakeX >= BOARD_WIDTH) || (snakeX < 0)
+					|| (snakeY >= BOARD_HEIGHT) || (snakeY < 0)) {
+				setTargetLocation();
+				snake.removeAll(snake);
+				snakeX = Integer.parseInt(config.getProperty("SNAKE_X"));
+				snakeY = Integer.parseInt(config.getProperty("SNAKE_Y"));
+				setDefaultDirection();
+			} else if ((snakeX == targetX) && ((snakeY == targetY))) {
 				setTargetLocation();
 			} else {
 				snake.remove(0);
@@ -121,8 +172,6 @@ public class GameBoard extends JPanel implements Runnable {
 	}
 
 	private void paintBoard(Graphics graphic) {
-		graphic.setColor(Color.RED);
-		graphic.fill3DRect(targetX, targetY, BOX_SIZE, BOX_SIZE, true);
 		for (int index = 0; index < snake.size(); index++) {
 			if ((snake.size() - 1) == index) {
 				graphic.setColor(Color.RED);
@@ -132,6 +181,8 @@ public class GameBoard extends JPanel implements Runnable {
 			SnakeBody snakeBody = snake.get(index);
 			snakeBody.drawSnake(graphic);
 		}
+		graphic.setColor(Color.RED);
+		graphic.fill3DRect(targetX, targetY, BOX_SIZE, BOX_SIZE, true);
 	}
 
 	@Override
@@ -170,6 +221,9 @@ public class GameBoard extends JPanel implements Runnable {
 				rightMove = false;
 				upMove = false;
 				downMove = true;
+			}
+			if (KeyEvent.VK_ESCAPE == key) {
+				System.exit(0);
 			}
 		}
 
